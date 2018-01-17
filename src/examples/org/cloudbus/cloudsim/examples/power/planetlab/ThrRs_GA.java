298a0937de084fe43e140be585eee2caaaf1fe79
zenara <zenaronline@gmail.com>
2018-01-17 18:25:46 +0530

    Static Threshold GA

---
+++ src/examples/org/cloudbus/cloudsim/examples/power/planetlab/ThrRs_GA.java
@@ -0,0 +1,55 @@
+package examples.org.cloudbus.cloudsim.examples.power.planetlab;
+
+import java.io.IOException;
+import org.cloudbus.cloudsim.examples.power.planetlab.PlanetLabRunner;
+
+/**
+ * A simulation of a heterogeneous power aware data center that applies the Static Threshold (THR)
+ * VM allocation policy and Random Selection (RS) VM selection policy.
+ * 
+ * This example uses a real PlanetLab workload: 20110303.
+ * 
+ * The remaining configuration parameters are in the Constants and PlanetLabConstants classes.
+ * 
+ * If you are using any algorithms, policies or workload included in the power package please cite
+ * the following paper:
+ * 
+ * Anton Beloglazov, and Rajkumar Buyya, "Optimal Online Deterministic Algorithms and Adaptive
+ * Heuristics for Energy and Performance Efficient Dynamic Consolidation of Virtual Machines in
+ * Cloud Data Centers", Concurrency and Computation: Practice and Experience, ISSN: 1532-0626, Wiley
+ * Press, New York, USA, 2011, DOI: 10.1002/cpe.1867
+ * 
+ * @author Anton Beloglazov
+ * @since Jan 5, 2012
+ */
+public class ThrRs_GA {
+
+	/**
+	 * The main method.
+	 * 
+	 * @param args the arguments
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public static void main(String[] args) throws IOException {
+		boolean enableOutput = true;
+		boolean outputToFile = false;
+		//String inputFolder = NonPowerAware.class.getClassLoader().getResource("workload/planetlab").getPath();
+		String inputFolder = "/Users/zenara/Documents/SUSL/Year 4 Sem 2/Research/Cloudsim/cloudsim3.0/cloudsim3.0/src/examples/workload/planetlab";
+                String outputFolder = "output";
+		String workload = "20110303"; // PlanetLab workload
+		String vmAllocationPolicy = "ga"; // Static Threshold (THR) VM allocation policy
+		String vmSelectionPolicy = "rs"; // Random Selection (RS) VM selection policy
+		String parameter = "0.8"; // the static utilization threshold
+
+		new PlanetLabRunner(
+				enableOutput,
+				outputToFile,
+				inputFolder,
+				outputFolder,
+				workload,
+				vmAllocationPolicy,
+				vmSelectionPolicy,
+				parameter);
+	}
+
+}
